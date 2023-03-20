71ecdd4c7126b6b8d410146a3dbea90525912662
captainalabi <52130306+captainalabi@users.noreply.github.com>
2023-03-20 13:11:14 +0100

    Add files via upload

---
+++ demo-spring-boot-internals/src/main/java/com/collabo/bootDemo/DemoSpringBootInternalsApplication.java
@@ -0,0 +1,13 @@
+package com.collabo.bootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootInternalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootInternalsApplication.class, args);
+	}
+
+}
